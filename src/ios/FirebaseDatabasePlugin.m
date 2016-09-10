54046fcf3d82d93c93e286a6f5a809189247cb68
Carlos Lawton <blakgeek@gmail.com>
2016-09-09 23:20:31 -0400

    Added support for most functions.  Still gotta decide how off will work.

--- src/ios/FirebaseDatabasePlugin.m
+++ src/ios/FirebaseDatabasePlugin.m
@@ -8,23 +8,114 @@
     if (![FIRApp defaultApp]) {
         [FIRApp configure];
     }
+    self.database = [FIRDatabase database];
+}
+
+- (void)push:(CDVInvokedUrlCommand *)command {
+
+    NSString *path = [command argumentAtIndex:0 withDefault:@"/" andClass:[NSString class]];
+    id value = [command argumentAtIndex:1];
+    FIRDatabaseReference *ref = [self.database referenceWithPath:path];
+
+    [[ref childByAutoId] setValue:value withCompletionBlock:^(NSError *error, FIRDatabaseReference *ref) {
+        CDVPluginResult *pluginResult;
+        if (error) {
+
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary:@{
+                    @"code" : @(error.code),
+                    @"message" : error.description
+            }];
+        } else {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[NSString stringWithFormat:@"%@/%@", path, [ref key]]];
+        }
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+}
+
+- (void)set:(CDVInvokedUrlCommand *)command {
+
+    NSString *path = [command argumentAtIndex:0 withDefault:@"/" andClass:[NSString class]];
+    id value = [command argumentAtIndex:1];
+    FIRDatabaseReference *ref = [self.database referenceWithPath:path];
+
+    [ref setValue:value withCompletionBlock:^(NSError *error, FIRDatabaseReference *ref) {
+        CDVPluginResult *pluginResult;
+        if (error) {
+
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary:@{
+                    @"code" : @(error.code),
+                    @"message" : error.description
+            }];
+        } else {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:path];
+        }
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+}
+
+- (void)update:(CDVInvokedUrlCommand *)command {
+
+    NSString *path = [command argumentAtIndex:0 withDefault:@"/" andClass:[NSString class]];
+    NSDictionary *values = [command argumentAtIndex:1 withDefault:@{} andClass:[NSDictionary class]];
+    FIRDatabaseReference *ref = [self.database referenceWithPath:path];
+
+    [ref updateChildValues:values withCompletionBlock:^(NSError *error, FIRDatabaseReference *ref) {
+        CDVPluginResult *pluginResult;
+        if (error) {
+
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary:@{
+                    @"code" : @(error.code),
+                    @"message" : error.description
+            }];
+        } else {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:path];
+        }
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
+}
+
+- (void)remove:(CDVInvokedUrlCommand *)command {
+
+    NSString *path = [command argumentAtIndex:0 withDefault:@"/" andClass:[NSString class]];
+    FIRDatabaseReference *ref = [self.database referenceWithPath:path];
+
+    [ref removeValueWithCompletionBlock:^(NSError *error, FIRDatabaseReference *ref) {
+        CDVPluginResult *pluginResult;
+        if (error) {
+
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary:@{
+                    @"code" : @(error.code),
+                    @"message" : error.description
+            }];
+        } else {
+            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+        }
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
 }
 
 - (void)once:(CDVInvokedUrlCommand *)command {
 
-    FIRDatabase *db = [FIRDatabase database];
-    NSLog(@"app: %@", [[[db app] options] databaseURL]);
-    FIRDataEventType type = [self stringToType:[command argumentAtIndex:0 withDefault:@"value" andClass:[NSString class]]];
-    NSString *path = [command argumentAtIndex:1 withDefault:@"/" andClass:[NSString class]];
-    FIRDatabaseReference *ref = [db referenceWithPath:path];
-    NSLog(@"path: %@", [ref URL]);
-    [ref observeSingleEventOfType:type withBlock:^(FIRDataSnapshot *_Nonnull snapshot) {
+    NSString *path = [command argumentAtIndex:0 withDefault:@"/" andClass:[NSString class]];
+    NSString *orderByType = [command argumentAtIndex:1 withDefault:nil andClass:[NSString class]];
+    NSString *orderByPath = [command argumentAtIndex:2 withDefault:nil andClass:[NSString class]];
+    NSDictionary *filters = [command argumentAtIndex:3 withDefault:@{} andClass:[NSDictionary class]];
+    NSNumber *limitToFirst = [command argumentAtIndex:4 withDefault:nil andClass:[NSNumber class]];
+    NSNumber *limitToLast = [command argumentAtIndex:5 withDefault:nil andClass:[NSNumber class]];
+    FIRDataEventType type = [self stringToType:[command argumentAtIndex:6 withDefault:@"value" andClass:[NSString class]]];
+
+    FIRDatabaseReference *ref = [self.database referenceWithPath:path];
+    FIRDatabaseQuery *query = [self createRef:ref withOrderByType:orderByType andPath:orderByPath];
+    FIRDatabaseQuery *filteredQuery = [self filterQuery:query withFilters:filters];
+    FIRDatabaseQuery *limitedQuery = [self limitQuery:query toFirst:limitToFirst andLast:limitToLast];
+
+    [limitedQuery observeSingleEventOfType:type withBlock:^(FIRDataSnapshot *_Nonnull snapshot) {
 
         [snapshot value];
         NSDictionary *result;
         CDVPluginResult *pluginResult = [self snapshotToResult:snapshot];
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    }             withCancelBlock:^(NSError *_Nonnull error) {
+    }                      withCancelBlock:^(NSError *_Nonnull error) {
 
         CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary:@{
                 @"code" : @(error.code),
@@ -47,7 +138,7 @@
     NSString *id = [command argumentAtIndex:7 withDefault:nil andClass:[NSString class]];
 
 
-    FIRDatabaseReference *ref = [[FIRDatabase database].reference child:path];
+    FIRDatabaseReference *ref = [self.database.reference child:path];
     FIRDatabaseQuery *query = [self createRef:ref withOrderByType:orderByType andPath:orderByPath];
     FIRDatabaseQuery *filteredQuery = [self filterQuery:query withFilters:filters];
     FIRDatabaseQuery *limitedQuery = [self limitQuery:query toFirst:limitToFirst andLast:limitToLast];
@@ -59,14 +150,19 @@
         CDVPluginResult *pluginResult = [self snapshotToResult:snapshot];
         [pluginResult setKeepCallbackAsBool:YES];
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    } withCancelBlock:^(NSError *error) {
+    }              withCancelBlock:^(NSError *error) {
 
+        CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary:@{
+                @"code" : @(error.code),
+                @"message" : error.description
+        }];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
     }];
 }
 
 - (void)off:(CDVInvokedUrlCommand *)command {
 
-
+    [[self.database reference] removeAllObservers];
 }
 
 - (FIRDatabaseQuery *)limitQuery:query toFirst:(NSNumber *)limitToFirst andLast:(NSNumber *)limitToLast {
